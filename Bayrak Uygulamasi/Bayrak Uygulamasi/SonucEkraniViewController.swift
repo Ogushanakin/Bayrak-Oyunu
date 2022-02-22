c97e544463bef6c9ee56eb3b45fdfb419bf2d747
Oğuzhan Akın <55364051+Ogushanakin@users.noreply.github.com>
2022-02-22 16:18:23 +0300

    Add files via upload

---
+++ Bayrak Uygulamasi/Bayrak Uygulamasi/SonucEkraniViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SonucEkraniViewController.swift
+//  Bayrak Uygulamasi
+//
+//  Created by AKIN on 21.02.2022.
+//
+
+import UIKit
+
+class SonucEkraniViewController: UIViewController {
+
+    @IBOutlet weak var labelSonuc: UILabel!
+    
+    @IBOutlet weak var labelYuzde: UILabel!
+    
+    var dogruSayisi:Int?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.hidesBackButton = true
+        
+        if let d = dogruSayisi {
+            labelSonuc.text = "\(d) DOĞRU \(5-d) YANLIŞ"
+            labelYuzde.text = "% \(d*100/5) Başarı"
+        }
+
+    }
+    
+    @IBAction func tekrarDene(_ sender: Any) {
+        
+        navigationController?.popToRootViewController(animated: true)
+        
+        
+    }
+    
+}
